71d97b3c9fd6a5f66ffa3ca74e9732dc60ce4724
Tristen Thompson <thomptri002@warren.k12.in.us>
2024-01-11 12:13:17 -0500

    updates

--- 01_Strings/03_String Comparisons/Assignment.py
+++ 01_Strings/03_String Comparisons/Assignment.py
@@ -1,2 +1,7 @@
 #Ask the User for two bits of text 
 #Then print out whether those to bits of text are equal.  
+
+
+text1 = input("Pleasse print some text: ")
+text2 = input("Please print more text: ")
+print(text1 == text2)
---
+++ 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+sep = " "
+
+full_name = sep.join([first_name, last_name])
+print(full_name)
