abf1a4c1cf88e3c028612872c26d555dfa8c8e9c
Tristen Thompson <thomptri002@warren.k12.in.us>
2024-02-02 11:58:07 -0500

    1.5.2 assingment

--- 01_Strings/05_Search Strings/Assignment.py
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -1,21 +1,8 @@
-# import sys
+text = ""
 
-# user_name = input('What is your name? ')
-# if user_name == '':
-#   print('Empty name? I cannot work with that. I am closing the program. Bye!')
-#   sys.exit()
-# print('Hello,', user_name)
-# print('Let us get started...')
+result = text.find("the")
 
-# #use Control+C to perform a KeyboardInterrupt
-# while True:
-#   print('hi!')
-
-# programming_languages = ["Java", "Python", "C++"]
-# print(programming_languages[10])
-
-# ages = {'Jim': 30, 'Pam': 28, 'Kevin': 33}
-# ages['Michael']
-
-# age = input('What is your age? ')
-# print('In 10 years, you will be', age + 10)
+if result == -1:
+    print("The word is not in the string")
+    print("It is located at",)
+    print("The word is after the ,")
