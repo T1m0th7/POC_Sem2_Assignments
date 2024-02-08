dbe84c44bab17daa9d11da3a3484ec82731d7860
Tristen Thompson <thomptri002@warren.k12.in.us>
2024-02-08 11:46:23 -0500

    01

---
+++ 05_More on Exceptions/01_Else and Finally/01_assingment.py
@@ -0,0 +1,8 @@
+def average(a : int, b: int) -> int: 
+    return (a+b) / 2
+
+
+
+number1 : float = float(input("Enter a decimal number: "))
+number2 : float = float(input("Enter another decimal number: "))
+print(average(number1, number2))
