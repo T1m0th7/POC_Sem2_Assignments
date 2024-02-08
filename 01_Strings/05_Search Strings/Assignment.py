42609e22d7b86bc2031b09506cfa1d85f32fd78c
Tristen Thompson <thomptri002@warren.k12.in.us>
2024-02-08 12:03:25 -0500

    assingment.py

--- 01_Strings/05_Search Strings/Assignment.py
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -1,8 +1,9 @@
-text = ""
+text = input("Enter some text please :) ")
 
 result = text.find("the")
 
 if result == -1:
-    print("The word is not in the string")
-    print("It is located at",)
-    print("The word is after the ,")
+    print("The word the is in the string")
+else:
+    print("The word the is not in the string")
+    print("It is located at",result)
