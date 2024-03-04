807959cfd450b68d08696f97b6853b83b2f0e9f2
Tristen Thompson <thomptri002@warren.k12.in.us>
2024-03-04 12:22:30 -0500

    UPDATES

--- 08_Object-Oriented Programming/assingment.py
+++ 08_Object-Oriented Programming/assingment.py
@@ -1,13 +1,43 @@
-class RightTriangle: 
+from math import hypot, atan, degrees, pi
+
+class RightTriangle:
     def __init__(self, base, height):
         self.base = base
-        self.height = height
-    
+        self.height = height   
+        self.min_angle = atan(self.height / self.base)
+        self.max_angle = pi / 2 - self.min_angle
+        self.min_angle = degrees(self.min_angle)
+        self.max_angle = degrees(self.max_angle)
+        small = self.min_angle
+        large = self.max_angle
+        self.min_angle = min(small, large)
+        self.max_angle = max(small, large)
+
+
     def area(self):
         return (1/2) * self.base * self.height
 
-triangle_1 = RightTriangle(3, 4)
-print("The area of triange_1 is", triangle_1.area())
+    
+    def hypotenuse(self):
+        return hypot(self.base, self.height)
 
-triangle_2 = RightTriangle(6, 8)
-print("The area of triangle_2 is", triangle_2.area())
+    def perimeter(self):
+        return self.base + self.height + self.hypotenuse()
+    
+
+
+
+triangle1 = RightTriangle(3, 4)
+print("The area of triangle1 is", triangle1.area())
+print("The hypotenuse of triangle1 is", triangle1.hypotenuse())
+print("The perimeter of triangle1 is", triangle1.perimeter())
+print("The small angle of triangle1 is", triangle1.min_angle)
+print("The large angle of triangle1 is", triangle1.max_angle)
+
+triangle2 = RightTriangle(30, 15)
+print("The area of triangle2 is", triangle2.area())
+print("The hypotenuse of triangle2 is", triangle2.hypotenuse())
+print("The perimeter of triangle2 is", triangle2.perimeter())
+print("The small angle of triangle2 is", triangle2.min_angle)
+print("The large angle of triangle2 is", triangle2.max_angle)
+triangle2.base = 15
