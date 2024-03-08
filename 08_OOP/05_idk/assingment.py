0651c2fd41ea7d45f52f8be70328529d9d28fdbc
Tristen Thompson <thomptri002@warren.k12.in.us>
2024-03-08 12:05:27 -0500

    updates

--- 08_OOP/05_idk/assingment.py
+++ 08_OOP/05_idk/assingment.py
@@ -1,51 +1,47 @@
-class Rectangle():
-    __counter = 0
-    
-    def get_count():
-        return Rectangle.__counter
-    
-    
+class Rectangle:
+
     def __init__(self, base: float, height: float) -> None:
-        if(base < 0):
+        if (base < 0):
             self.__base = 0
-        else:       
+        else:
             self.__base = base
-            
-        if(height < 0):
+
+        if (height < 0):
             self.__height = 0
         else:
             self.__height = height
 
-        Rectangle.__counter += 1
-
-        
-        
     def get_height(self) -> float:
         return self.__height
-    
+
     def get_base(self) -> float:
-        return self.__base                              #YOUDO the get_base method
-    
+        return self.__base 
+
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
-    
+
     def get_area(self) -> float:
-        return self.__base * self.__height                #Youdo get_area method
- 
+        return self.__base * self.__height       # Youdo get_area method
+    
+    def __str__(self) -> str:
+        # Rectangle of base:3, height:4
+        return "Rectangle of base:" + str(self.__base) + ", height:" + str(self.__height)
+        
 rectangle1 = Rectangle(3, 4)
 print(rectangle1.get_base())
 print(rectangle1.get_height())
 print(rectangle1.get_area())
 print(rectangle1.get_perimeter())
 
+print(rectangle1)
+
 rectangle2 = Rectangle(5, 6)
 print(rectangle2.get_base())
 print(rectangle2.get_height())
 print(rectangle2.get_area())
 print(rectangle2.get_perimeter())
- 
 
+print(rectangle2)
 
-print(Rectangle.get_count())
-#YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-#using only the methods not the fields/property/attributes
+# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
+# using only the methods not the fields/property/attributes
